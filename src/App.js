8725fa2398e0f78fe52b347177162734fb66e3ea
Амир Джалилов <amir.rima@bk.ru>
2021-11-07 21:34:06 +0300

    VK

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import {Container, Row, Col} from 'react-bootstrap';
+import VkAuth from 'react-vk-auth';
 import './App.css';
 
 
 function App() {
+  let handleVkResponse = (data) => {
+    console.warn(data)
+    console.log(data)
+  }
   return (
       <Container className="container-md" style={{width: 800, height: 600}}>
         <Row className="align-items-md-center h-50 justify-content-md-center">
@@ -20,6 +25,7 @@ function App() {
         <Row className="align-items-md-center h-50 justify-content-md-center" style={{backgroundColor: "#FF0"}}>
 
         </Row>
+        <VkAuth apiId="7995678" callback={handleVkResponse} />
       </Container>
   );
 }
