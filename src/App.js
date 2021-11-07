7d4b68e3764b020134cb0f6702a202195e4d392c
Амир Джалилов <amir.rima@bk.ru>
2021-11-07 22:56:21 +0300

    Google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {Container, Row, Col} from 'react-bootstrap';
 import VkAuth from 'react-vk-auth';
+import GoogleLogin from 'react-google-login';
+
 import './App.css';
 
 
 function App() {
+  const [session, setSession] = useState();
+
   let handleVkResponse = (data) => {
-    console.warn(data)
-    console.log(data)
+    if(data.status === "connected")
+    {
+        setSession(data.session)
+    }
+  }
+  const responseGoogle = (response) => {
+    console.log(response);
   }
   return (
       <Container className="container-md" style={{width: 800, height: 600}}>
@@ -23,9 +32,25 @@ function App() {
           </Row>
         </Row>
         <Row className="align-items-md-center h-50 justify-content-md-center" style={{backgroundColor: "#FF0"}}>
-
+        {
+            !session && <>
+                <VkAuth apiId="7995678" callback={handleVkResponse} />
+                <GoogleLogin 
+                    clientId="516879760228-3uue835hocpo9ce8qae791e0kqt36h24.apps.googleusercontent.com"
+                    buttonText="Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </>
+        }
+        {
+            session && <>
+                <p>{session.user.first_name} {session.user.last_name}</p>
+            </>
+        }
         </Row>
-        <VkAuth apiId="7995678" callback={handleVkResponse} />
+        
       </Container>
   );
 }
